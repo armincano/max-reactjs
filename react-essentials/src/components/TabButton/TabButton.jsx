c6d6fdbae30feca662fc40b7328d47f010ab8317
Armin Cano <arminescano@gmail.com>
2025-01-05 01:22:15 -0300

    feat: add conditional styling when button is clicked

--- react-essentials/src/components/TabButton/TabButton.jsx
+++ react-essentials/src/components/TabButton/TabButton.jsx
@@ -1,8 +1,7 @@
-export default function TabButton({children, onClick}) {
-
-  return (
-    <li>
-      <button onClick={onClick}>{children}</button>
-    </li>
-  );
+export default function TabButton({ children, onClick, isSelected }) {
+	return (
+		<li>
+			<button className={isSelected ? 'active' : undefined} onClick={onClick}>{children}</button>
+		</li>
+	);
 }
