e7b27a98368297aedffa60ccf32c3c467cc47e1d
Armin Cano <arminescano@gmail.com>
2025-01-04 23:04:24 -0300

    feat: add state managment for content when clicking button

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
+
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+	const [selectedTopic, setSelectedTopic] = useState("components");
+
+	function handleClick(selectedButton) {
+		setSelectedTopic(selectedButton);
+	}
+
 	return (
 		<div>
 			<Header />
@@ -20,11 +29,20 @@ function App() {
 				<section id="examples">
 					<h2>Examples</h2>
 					<menu>
-						<TabButton>Components</TabButton>
-						<TabButton>JSX</TabButton>
-						<TabButton>Props</TabButton>
-						<TabButton>State</TabButton>
+						<TabButton onClick={() => handleClick("components")}>
+							Components
+						</TabButton>
+						<TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
+						<TabButton onClick={() => handleClick("props")}>Props</TabButton>
+						<TabButton onClick={() => handleClick("state")}>State</TabButton>
 					</menu>
+					<div id="tab-content">
+						<h3>{EXAMPLES[selectedTopic].title}</h3>
+						<p>{EXAMPLES[selectedTopic].description}</p>
+						<pre>
+							<code>{EXAMPLES[selectedTopic].code}</code>
+						</pre>
+					</div>
 				</section>
 			</main>
 		</div>
